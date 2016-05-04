7df1ccca4ce48d2437fbdc2ad88ea6489f2b1fcc
Thao Dinh Hoang Phuong <thao.dinh35@titansoft.com.sg>
2016-05-05 00:03:35 +0800

    Make local variable BookSeries a field as there is no use for it in test method now

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -8,18 +8,18 @@ namespace PotterShoppingCart.Tests
     [TestClass]
     public class PotterShoppingCartTest
     {
+        private List<Book> _BookSeries = new List<Book>
+        {
+            new Book{ Number = 1, Price = 100m},
+            new Book{ Number = 2, Price = 100m},
+            new Book{ Number = 3, Price = 100m},
+            new Book{ Number = 4, Price = 100m},
+            new Book{ Number = 5, Price = 100m}
+        };
+
         [TestMethod]
         public void Test_Buy_2_Different_Books_Will_Cost_190()
         {
-            List<Book> BookSeries = new List<Book>
-            {
-                new Book{ Number = 1, Price = 100m},
-                new Book{ Number = 2, Price = 100m},
-                new Book{ Number = 3, Price = 100m},
-                new Book{ Number = 4, Price = 100m},
-                new Book{ Number = 5, Price = 100m}
-            };
-
             List<Book> Basket = new List<Book>
             {
                 new Book {Number = 3, Price = 100m},

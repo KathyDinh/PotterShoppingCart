f78a55ca5eca529717caaa2a2f56775c358e79a8
Thao Dinh Hoang Phuong <thao.dinh35@titansoft.com.sg>
2016-05-05 00:15:53 +0800

    Rename property name Number to Volume. Add 2nd test for 2 copies of the same volume will get 200. 2nd test fails.

--- PotterShoppingCart.Tests/PotterShoppingCartTest.cs
+++ PotterShoppingCart.Tests/PotterShoppingCartTest.cs
@@ -10,26 +10,43 @@ namespace PotterShoppingCart.Tests
     {
         private List<Book> _BookSeries = new List<Book>
         {
-            new Book{ Number = 1, Price = 100m},
-            new Book{ Number = 2, Price = 100m},
-            new Book{ Number = 3, Price = 100m},
-            new Book{ Number = 4, Price = 100m},
-            new Book{ Number = 5, Price = 100m}
+            new Book{ Volume = 1, Price = 100m},
+            new Book{ Volume = 2, Price = 100m},
+            new Book{ Volume = 3, Price = 100m},
+            new Book{ Volume = 4, Price = 100m},
+            new Book{ Volume = 5, Price = 100m}
         };
 
         [TestMethod]
-        public void Test_Buy_2_Different_Books_Will_Cost_190()
+        public void Test_Buy_2_Different_Volumes_Will_Cost_190()
         {
             List<Book> Basket = new List<Book>
             {
-                new Book {Number = 3, Price = 100m},
-                new Book {Number = 5, Price = 100m}
+                new Book {Volume = 3, Price = 100m},
+                new Book {Volume = 5, Price = 100m}
             };
 
             var target = new Cashier();
             var actual = target.GetTotalCostOf(Basket);
 
             var expected = 190m;
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void Test_Buy_2_Copies_Of_Same_Volume_Will_Cost_200()
+        {
+
+            List<Book> Basket = new List<Book>
+            {
+                new Book {Volume = 3, Price = 100m},
+                new Book {Volume = 3, Price = 100m}
+            };
+
+            var target = new Cashier();
+            var actual = target.GetTotalCostOf(Basket);
+
+            var expected = 200m;
             Assert.AreEqual(expected, actual);
         }
     }
@@ -44,7 +61,7 @@ namespace PotterShoppingCart.Tests
 
     public class Book
     {
-        public int Number { get; set; }
+        public int Volume { get; set; }
         public decimal Price { get; set; }
     }
 }
